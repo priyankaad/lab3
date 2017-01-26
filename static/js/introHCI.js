fa81e1df80fb175beb536ef71758811ee063ba4c
Priyanka Dasgupta <pdasgupt@ucsd.edu>
2017-01-25 20:57:42 -0800

    modified projectClick

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -23,9 +23,18 @@ function projectClick(e) {
     // In an event handler, $(this) refers to      
     // the object that triggered the event      
     $(this).css("background-color", "#7fff00");
-  var containingProject = $(this).closest(".project");
-    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
-}
+
+ var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+} 
+ // var containingProject = $(this).closest(".project");
+   // containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
 
    	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
