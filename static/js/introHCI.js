4343ee5c1241d0671955d22a745173cf4bd83271
Priyanka Dasgupta <pdasgupt@ucsd.edu>
2017-01-25 21:05:48 -0800

    modified projectClick

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -31,6 +31,7 @@ function projectClick(e) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
        description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    $("myelement").hide()
     }
 } 
  // var containingProject = $(this).closest(".project");
